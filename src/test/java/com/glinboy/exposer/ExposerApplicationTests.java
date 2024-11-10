827003920ead8c94c2ac6b90d075e5829deeafa2
GLinBoy <glinboy@live.com>
2024-11-10 15:21:48 +0100

    🕯 START: Initiated the project
    resolved #1

---
+++ src/test/java/com/glinboy/exposer/ExposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.exposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
