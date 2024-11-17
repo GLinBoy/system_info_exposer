42949c6a7e83e1b1a9f2634f9e2c78e6ce4bd7f4
GLinBoy <glinboy@live.com>
2024-11-17 10:29:54 +0100

    📦 NEW: Add OpenAPI config file
    resolved #22

---
+++ src/main/java/com/glinboy/exposer/config/OpenApiConfig.java
@@ -0,0 +1,46 @@
+package com.glinboy.exposer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Value("${spring.application.name}")
+	private String name;
+	
+	@Value("${application.info.description}")
+	private String description;
+	
+	@Value("${application.info.version}")
+	private String version;
+	
+	@Value("${application.info.license}")
+	private String license;
+	
+	@Value("${application.info.license-url}")
+	private String licenseUrl;
+	
+	@Value("${application.info.github-url}")
+	private String githubUrl;
+
+	@Bean
+	OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title(name)
+						.description(description)
+						.version(version)
+						.license(new License().name(license)
+								.url(licenseUrl))
+						)
+				.externalDocs(new ExternalDocumentation()
+						.description("Source code")
+						.url(githubUrl));
+	}
+}
