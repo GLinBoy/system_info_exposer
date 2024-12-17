9feb0f209f8413fb718f36a0a682084f318cc982
GLinBoy <glinboy@live.com>
2024-12-17 15:02:38 +0100

    📦 NEW: Add SecurityConfig and configure security

---
+++ src/main/java/com/glinboy/exposer/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.glinboy.exposer.security;
+
+import java.util.stream.Stream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig {
+
+	private static final String[] AUTH_WHITELIST = {
+			"/",
+			// -- Swagger UI v3 (OpenAPI)
+			"/swagger-ui.html",
+			"/v3/api-docs/**",
+			"/swagger-ui/**"
+		};
+
+	@Bean
+	SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.cors(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
+				.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+				.authorizeHttpRequests(request -> 
+					request.requestMatchers(Stream.of(AUTH_WHITELIST)
+							.map(AntPathRequestMatcher::new)
+							.toList()
+							.toArray(new AntPathRequestMatcher[] {}))
+						.permitAll()
+						.anyRequest().authenticated())
+				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
+	}
+}
