744cd533864bea2cc64cce9c0461e8b18b0f3d0d
GLinBoy <glinboy@live.com>
2024-12-18 14:07:59 +0100

    👌 IMPROVE: Mark endpoints to allow Swagger UI use API key

--- src/main/java/com/glinboy/exposer/web/rest/EnvironmentResource.java
+++ src/main/java/com/glinboy/exposer/web/rest/EnvironmentResource.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
+@SecurityRequirement(name = "API Key Authentication")
 @RequestMapping(value = {"/api/v1/environment", "/api/v1/env"})
 public class EnvironmentResource {
 
