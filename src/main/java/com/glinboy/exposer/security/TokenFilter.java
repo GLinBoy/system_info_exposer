d03cb0b1d4858f15a7959a70cc346fb314f7eeaa
GLinBoy <glinboy@live.com>
2024-12-18 14:01:58 +0100

    👌 IMPROVE: Generate a token (a UUID) if token is empty at application.yml

--- src/main/java/com/glinboy/exposer/security/TokenFilter.java
+++ src/main/java/com/glinboy/exposer/security/TokenFilter.java
@@ -2,6 +2,7 @@ package com.glinboy.exposer.security;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,19 +11,30 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class TokenFilter extends OncePerRequestFilter {
 
 	@Value("${application.api.token-name:X-API-KEY}")
 	private String tokenName;
 
-	@Value("${application.api.token-value:123XYZ}")
+	@Value("${application.api.token-value:}")
 	private String tokenValue;
+	
+	@PostConstruct
+  private void initialize() {
+      if (tokenValue == null || tokenValue.isEmpty()) {
+          tokenValue = UUID.randomUUID().toString();
+          log.info("A token generated: {}", tokenValue);
+      }
+  }
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
